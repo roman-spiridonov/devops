a665953ba5830588caae5f0f8bf3a9fcebfc848e
RomaRS <romars@phystech.edu>
2016-01-03 23:51:59 +0400

    updated init.el for emacs to recognize some hotkeys like shift + arrows

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,4 +1,4 @@
-;; -*- mode: emacs-lisp -*-
+;; -*- mode: emacs-lisp -*
 ;; Simple .emacs configuration
 
 ;; ---------------------
@@ -18,7 +18,7 @@
 (require 'compile)
 (ido-mode t)
 (menu-bar-mode -1)
-(normal-erase-is-backspace-mode 1)
+(normal-erase-is-backspace-mode 0) ; IMPORTANT FOR CYGWIN
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
 (setq column-number-mode t)
@@ -34,19 +34,18 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit autoface-default :strike-through nil :underline nil :slant normal :weight normal :height 120 :width normal :family "monaco"))))
- '(column-marker-1 ((t (:background "red"))))
- '(diff-added ((t (:foreground "cyan"))))
- '(flymake-errline ((((class color) (background light)) (:background "Red"))))
+ '(column-marker-1 ((t (:background "red"))) t)
+ '(diff-added ((t (:foreground "cyan"))) t)
+ '(flymake-errline ((((class color) (background light)) (:background "Red"))) t)
  '(font-lock-comment-face ((((class color) (min-colors 8) (background light)) (:foreground "red"))))
- '(fundamental-mode-default ((t (:inherit default))))
+ '(fundamental-mode-default ((t (:inherit default))) t)
  '(highlight ((((class color) (min-colors 8)) (:background "white" :foreground "magenta"))))
  '(isearch ((((class color) (min-colors 8)) (:background "yellow" :foreground "black"))))
  '(linum ((t (:foreground "black" :weight bold))))
  '(region ((((class color) (min-colors 8)) (:background "white" :foreground "magenta"))))
  '(secondary-selection ((((class color) (min-colors 8)) (:background "gray" :foreground "cyan"))))
- '(show-paren-match ((((class color) (background light)) (:background "black"))))
- '(vertical-border ((t nil)))
-)
+ '(show-paren-match ((((class color) (background light)) (:background "black"))) t)
+ '(vertical-border ((t nil))))
 
 ;; ------------
 ;; -- Macros --
@@ -61,7 +60,7 @@
 (global-set-key "\M-o" 'other-window)
 (global-set-key "\M-i" 'back-window)
 (global-set-key "\C-z" 'zap-to-char)
-(global-set-key "\C-h" 'backward-delete-char)
+;(global-set-key "\C-h" 'backward-delete-char)
 (global-set-key "\M-d" 'delete-word)
 (global-set-key "\M-h" 'backward-delete-word)
 (global-set-key "\M-u" 'zap-to-char)
@@ -75,3 +74,48 @@
 (require 'jade-mode)    
 (add-to-list 'auto-mode-alist '("\\.styl$" . sws-mode))
 (add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
+
+(add-to-list 'custom-theme-load-path "~/emacs-color-theme-solarized")
+(load-theme 'solarized t)
+;;(require 'color-theme)
+;;(color-theme-initialize)
+;;(color-theme-hober)
+;;(require 'color-theme-solarized)
+;;(color-theme-solarized-dark)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(frame-background-mode (quote dark)))
+
+;; Shift+arrow key support in screen
+(define-key global-map "\e[1;4D" (kbd "M-S-<left>"))
+(define-key global-map "\e[1;4C" (kbd "M-S-<right>"))
+(define-key global-map "\e[1;4A" (kbd "M-S-<up>"))
+(define-key global-map "\e[1;4B" (kbd "M-S-<down>"))
+
+(define-key global-map "\e[1;2D" (kbd "S-<left>"))
+(define-key global-map "\e[1;2C" (kbd "S-<right>"))
+(define-key global-map "\e[1;2A" (kbd "S-<up>"))
+(define-key global-map "\e[1;2B" (kbd "S-<down>"))
+
+;; C-Home, C-End (+Shift)
+(define-key global-map "\e[1;5F" (kbd "C-<end>"))
+(define-key global-map "\e[1;5H" (kbd "C-<home>"))
+(define-key global-map "\e[1;6F" (kbd "C-S-<end>"))
+(define-key global-map "\e[1;6H" (kbd "C-S-<home>"))
+
+;; C-Left, C-Right (+Shift)
+(define-key global-map "\e[1;5C" (kbd "M-f"))
+(define-key global-map "\e[1;5D" (kbd "M-b"))
+(define-key global-map "\e[1;6C" (kbd "M-S-f"))
+(define-key global-map "\e[1;6D" (kbd "M-S-b"))
+
+;; C-Up, C-Down
+(global-set-key (kbd "\e[1;5A") 'backward-paragraph)
+(global-set-key (kbd "\e[1;5B") 'forward-paragraph)
+
+;; Page-Up, Page-Down
+(global-set-key (kbd "\e[1;5~") 'scroll-up)
+(global-set-key (kbd "\e[1;6~") 'scroll-down)
